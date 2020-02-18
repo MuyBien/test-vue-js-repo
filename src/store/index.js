519c370d5e87cc9f8877c78640935dc68a20b8eb
MuyBien <montanana.frederic@gmail.com>
2020-02-18 18:53:34 +0100

    Créé un store avec les joueurs de la Ligue 1 Conforama

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,29 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+import Players from "@/assets/players/fr.json";
+const positionsMap = {
+    goalkeeper: ["G"],
+    backer: ["DL", "DC"],
+    middle: ["MD", "MO"],
+    forward: ["A"],
+};
+
 export default new Vuex.Store({
     state: {
+        players: Players,
     },
     mutations: {
+    },
+
+    getters: {
+        playersByPosition: function (state) {
+            return function (position) {
+                return state.players.filter(function (player) {
+                    return positionsMap[position].includes(player.position);
+                });
+            };
+        },
     },
     actions: {
     },
