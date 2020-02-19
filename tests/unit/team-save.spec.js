3a0886e12001ba54f5fd09c37cc5a1e3fe2a3638
MuyBien <montanana.frederic@gmail.com>
2020-02-19 23:18:56 +0100

    Teste le chargement du nom des joueurs

--- tests/unit/team-save.spec.js
+++ tests/unit/team-save.spec.js
@@ -12,10 +12,10 @@ describe("Chargement depuis DB", () => {
     it("Charge une équipe complète", () => {
         let team = {
             starters: [
-                { index: 0, position: "backer", note: 6, goals: 2, csc: 1 },
+                { index: 0, position: "backer", name: "Abdelhamid", note: 6, goals: 2, csc: 1 },
             ],
             substitutes: [
-                { index: 0, position: "middle", note: 2, goals: 1, csc: 0 },
+                { index: 0, position: "middle", name: "Sanson", note: 2, goals: 1, csc: 0 },
             ],
             substitutions: [
                 { index: 0, starter: 2, substitute: 1, note: 8 },
@@ -23,11 +23,13 @@ describe("Chargement depuis DB", () => {
         };
         componentWrapper.vm.loadTeam(team);
         expect(componentWrapper.vm.$data.starters[0].position).to.be.equals("backer");
+        expect(componentWrapper.vm.$data.starters[0].name).to.be.equals("Abdelhamid");
         expect(componentWrapper.vm.$data.starters[0].note).to.be.equals(6);
         expect(componentWrapper.vm.$data.starters[0].csc).to.be.equals(1);
         expect(componentWrapper.vm.$data.starters[0].goals).to.be.equals(2);
 
         expect(componentWrapper.vm.$data.substitutes[0].position).to.be.equals("middle");
+        expect(componentWrapper.vm.$data.substitutes[0].name).to.be.equals("Sanson");
         expect(componentWrapper.vm.$data.substitutes[0].note).to.be.equals(2);
         expect(componentWrapper.vm.$data.substitutes[0].csc).to.be.equals(0);
         expect(componentWrapper.vm.$data.substitutes[0].goals).to.be.equals(1);
