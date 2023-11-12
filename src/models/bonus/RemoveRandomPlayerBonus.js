a67d192e1b1ebe7ad32cf85007140f64dbbcae4c
MuyBien <montanana.frederic@gmail.com>
2023-11-12 17:10:03 +0100

    Gère le bonus Chapron Rouge

--- src/models/bonus/RemoveRandomPlayerBonus.js
+++ src/models/bonus/RemoveRandomPlayerBonus.js
@@ -1,18 +1,39 @@
 import { Bonus } from "./Bonus";
+import { Rotaldo } from "../players/Rotaldo";
 
 export class RemoveRandomPlayerBonus extends Bonus {
 
-  constructor () {
+  playerId;
+
+  constructor (bonusData) {
     super({
       name: "Chapron Rouge",
       value: "removeRandomPlayer",
       icon: "/img/bonus-images/chapron-rouge.png",
       description: "Remplace un joueur de champ final (après les remplacements) par un Rotaldo.",
-      timing: "after",
+      timing: "afterSubstitutions",
+      isLiveApplied: false,
     });
+    this.playerId = bonusData.playerId;
   }
 
   apply (team, opponentTeam) {
-    //TODO
+    const replacePlayer = (players) => {
+      const playerIndex = players.findIndex(p => p.playerId === this.playerId);
+      if (playerIndex !== - 1) {
+        const rotaldoAmount = players.filter(player => player.lastName === "Rotaldo").length;
+        players[playerIndex] = new Rotaldo({
+          ...players[playerIndex],
+          ownGoals: (rotaldoAmount + 1) % 3 ? 0 : 1,
+          isSubstitute: true,
+        });
+        return true; // Joueur trouvé et remplacé
+      }
+      return false; // Joueur non trouvé
+    };
+
+    if (! replacePlayer(team.pitchPlayers)) {
+      replacePlayer(opponentTeam.pitchPlayers);
+    }
   }
 }
---
+++ src/models/bonus/__tests__/RemoveRandomPlayerBonus.test.js
@@ -0,0 +1,43 @@
+import { describe, it, expect } from "vitest";
+import { RemoveRandomPlayerBonus } from "../RemoveRandomPlayerBonus";
+import { Player } from "@/models/players/Player";
+import { Rotaldo } from "@/models/players/Rotaldo";
+
+const createMockTeam = (playerIds) => {
+  return { pitchPlayers: playerIds.map(id => new Player({ playerId: id })) };
+};
+
+describe("Le bonus RemoveRandomPlayerBonus", () => {
+  it("remplace un joueur spécifié par un Rotaldo dans l'équipe", () => {
+    const team = createMockTeam(["player1", "player2", "player3"]);
+    const opponentTeam = createMockTeam(["player4", "player5", "player6"]);
+
+    const bonus = new RemoveRandomPlayerBonus({ playerId: "player2" });
+    bonus.apply(team, opponentTeam);
+
+    expect(team.pitchPlayers.some(p => p instanceof Rotaldo)).toBe(true);
+  });
+
+  it("remplace un joueur spécifié par un Rotaldo dans l'équipe adverse", () => {
+    const team = createMockTeam(["player1", "player2", "player3"]);
+    const opponentTeam = createMockTeam(["player4", "player5", "player6"]);
+
+    const bonus = new RemoveRandomPlayerBonus({ playerId: "player5" });
+    bonus.apply(team, opponentTeam);
+
+    expect(opponentTeam.pitchPlayers.some(p => p instanceof Rotaldo)).toBe(true);
+  });
+
+  it("remplace un joueur spécifié par un Rotaldo avec un CSC i c'est le 3eme Rotaldo", () => {
+    const team = createMockTeam(["player1", "player2", "player3"]);
+    const opponentTeam = createMockTeam(["player4", "player5", "player6"]);
+    opponentTeam.pitchPlayers[0].lastName = "Rotaldo";
+    opponentTeam.pitchPlayers[2].lastName = "Rotaldo";
+
+    const bonus = new RemoveRandomPlayerBonus({ playerId: "player5" });
+    bonus.apply(team, opponentTeam);
+
+    expect(opponentTeam.pitchPlayers[1].ownGoals).toBe(1);
+  });
+
+});
