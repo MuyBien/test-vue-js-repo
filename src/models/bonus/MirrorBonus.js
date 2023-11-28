780975d3c8d302d4950cef8fdce58d3aa5dd9082
MuyBien <montanana.frederic@gmail.com>
2023-11-28 17:44:05 +0100

    Gère le miroir sur les bonus Mirroir et Chapron
    Aucun effet du miroir sur ces bonus

--- src/models/bonus/MirrorBonus.js
+++ src/models/bonus/MirrorBonus.js
@@ -1,18 +1,36 @@
+import { RemoveRandomPlayerBonus } from "@/models/bonus/RemoveRandomPlayerBonus";
 import { Bonus } from "./Bonus";
 
 export class MirrorBonus extends Bonus {
+
+  noEffectWith = [RemoveRandomPlayerBonus, MirrorBonus];
 
   constructor () {
     super({
       name: "Miroir",
       value: "mirror",
       icon: "/img/bonus-images/mirror.png",
-      description: "Le bonus de ton adversaire se retourne contre lui. MPG Calculator pourra bientôt calculer ce bonus selon les bonus restants à l'adversaire",
-      timing: "before",
+      description: "Le bonus de ton adversaire se retourne contre lui.",
+      timing: "beforeAll",
+      isLiveApplied: false,
     });
   }
 
-  apply () {
-    //TODO
+  apply (team, opponentTeam) {
+    if (this.noEffectWith.includes(opponentTeam.bonus)) {
+      return;
+    }
+
+    // Si le bonus est liveApplied il faut le désappliquer
+    // TODO sauf si le bonus est le même que celui appliqué au début !
+    if (opponentTeam.bonus.isLiveApplied) {
+      opponentTeam.bonus.revert(opponentTeam, team);
+    }
+
+    team.bonus.apply = opponentTeam.bonus.apply;
+    team.bonus.timing = opponentTeam.bonus.timing;
+    team.bonus.isLiveApplied = false;
+
+    opponentTeam.bonus.isLiveApplied = true;
   }
 }
