526825be779ca87b240dbc4062fd05394ec557ee
MuyBien <montanana.frederic@gmail.com>
2024-03-29 11:54:03 +0100

    Utilise la méthode module pour l'import de path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-const path = require("path");
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
